95a52ea44216697c5b52dfef85e705900d9d0d85
NaufalEka4371 <naufaleka1734@gmail.com>
2026-06-25 12:19:22 +0700

    Implement MQTT client for smart home status updates

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,61 @@
-test = "Hello, World!";
+const statusDiv = document.getElementById("status");
+
+const client = mqtt.connect(
+    "wss://broker.hivemq.com:8884/mqtt"
+);
+
+client.on("connect", () => {
+
+    console.log("MQTT Connected");
+
+    statusDiv.innerHTML = "MQTT : Connected";
+    statusDiv.className = "status online";
+
+    client.subscribe("smarthome/suhu");
+    client.subscribe("smarthome/kelembaban");
+    client.subscribe("smarthome/jarak");
+});
+
+client.on("reconnect", () => {
+
+    statusDiv.innerHTML = "MQTT : Reconnecting...";
+    statusDiv.className = "status offline";
+});
+
+client.on("error", (err) => {
+    console.log(err);
+});
+
+client.on("message", (topic, message) => {
+
+    let value = message.toString();
+
+    if(topic === "smarthome/suhu"){
+        document.getElementById("suhu").innerText = value;
+    }
+
+    if(topic === "smarthome/kelembaban"){
+        document.getElementById("kelembaban").innerText = value;
+    }
+
+    if(topic === "smarthome/jarak"){
+        document.getElementById("jarak").innerText = value;
+    }
+
+});
+
+function lampu1(status){
+    client.publish("smarthome/lampu1", status);
+}
+
+function lampu2(status){
+    client.publish("smarthome/lampu2", status);
+}
+
+function lampu3(status){
+    client.publish("smarthome/lampu3", status);
+}
+
+function kipas(status){
+    client.publish("smarthome/kipas", status);
+}
